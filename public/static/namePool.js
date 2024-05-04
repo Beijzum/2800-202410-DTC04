114a94771bab62a45ba254273d463720e8c5ceb0
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-05-03 21:06:43 -0700

    contribute to random pool

--- public/static/namePool.js
+++ public/static/namePool.js
@@ -1,3 +1,14 @@
-const firstNamesPool = [];
-const middleNamePool = [];
-const lastNamesPool = [];
+// WE GO BY USER NAMES (firstname = goku, lastname = drip, rng = 69 --> gokudrip69)
+
+// first names here
+const firstNamesPool = [
+    'poopy', 'butt', 'mature', 'epic', 'shitake',
+    // NAMES HERE
+];
+
+
+// last names
+const lastNamesPool = [
+    'mushroom', 'smear', 'monkey', 'kappa', 'popo',
+    // NAMES HERE
+];
