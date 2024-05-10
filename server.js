0dc1b588afc678c6a7f1be5453452290fe0f691f
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-05-10 11:22:35 -0700

    move session destroy into page route

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
 
+// import externals
 const express = require('express');
 const session = require("express-session");
+const cors = require("cors");
 const app = express();
+
+const database = require("./database");
+const schemas = require("./joiValidation");
+
+// set port
 const PORT = 3000;
 
 // requirements for websocket
@@ -12,24 +19,84 @@ const { Server } = require('socket.io');
 
 // requirements for geminiAI
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { secureHeapUsed } = require('crypto');
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-// requirements for sessions
+// session configuration
 app.use(session({
     secret: process.env.NODE_SESSION_SECRET,
     resave: true,
     saveUninitialized: false,
-    cookie: { secure: false }
+    cookie: { 
+        secure: false,
+        maxAge: 3 * 60 * 60 * 1000
+    },
+    store: database.mongoSessionStorage,
+    unset: "destroy"
 }))
 
 // set up ejs
 app.set("view engine", "ejs");
 
+// set up middleware
+app.use(express.json());
+app.use(cors());
+
 // set up routes
 app.use(express.static(__dirname + "/public"));
 app.use(require("./pageRoutes"));
 
-app.listen(PORT, () => {
-    console.log(`Now listening to port ${PORT}`);
+// GET ROUTES SECTION
+
+app.get("/logout", (req, res) => {
+    req.session.destroy();
+    req.session = null;
+    res.redirect("/");
 })
+
+
+
+// POST ROUTES SECTION
+
+app.post("/createAccount", async (req, res) => {
+    let validationResult = schemas.signUpSchema.validate(req.body);
+    if (validationResult.error) {
+        console.log(validationResult.error.message);
+    } else { 
+        let errorList = await database.signUpUser(req.body);
+        if (errorList.length === 0) {
+            req.session.username = req.body.username;
+            res.redirect("/");
+            return;
+        }
+        
+    }
+})
+
+app.post("/loginAccount", async (req, res) => {
+    let validationResult = schemas.loginSchema.validate(req.body);
+    if (validationResult.error) {
+        console.log("asdf");
+        console.log(validationResult.error.message);
+    } else { 
+        let loginResult = await database.loginUser(req.body);
+        if (loginResult) {
+            req.session.username = loginResult.username;
+            res.redirect("/");
+            return;
+        }
+        res.send(200)
+    }
+})
+
+startServer();
+
+async function startServer() {
+    let connection = await database.client.connect();
+    if (connection.topology.isConnected()) 
+        app.listen(PORT, () => { 
+            console.log(`Database succesfully connected, now listening to port ${PORT}`);
+        });
+    else console.log("Error, could not connect to database, to try again, restart the server.");
+}
