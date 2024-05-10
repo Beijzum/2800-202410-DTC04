e88ecb15df2dd3e771d5b2d63e78548bb75a4f38
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-05-10 03:07:09 -0700

    add page routing into seperate file

---
+++ pageRoutes.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.render("index", {authenticated: req.session.username !== undefined})
+});
+
+router.get("/game", (req, res) => {
+    // redirect to login if unauthenticated
+    if (req.session.username) res.render("game", {authenticated: true})
+    else res.redirect("/login");
+});
+
+router.get("/lobby", (req, res) => {
+    if (req.session.username) res.render("lobby", {authenticated: true});
+    else res.redirect("/login");
+});
+
+router.get("/login", (req, res) => {
+    if (req.session.username) res.redirect("/index");
+    else res.render("login", {authenticated: false});
+})
+
+router.get("/signup", (req, res) => {
+    if (req.session.username) res.redirect("/index");
+    else res.render("signup", {authenticated: false});
+})
+
+router.get("/profile", (req, res) => {
+    if (req.session.username) res.render("profile", {authenticated: true});
+    else redirect("/login");
+})
+
+router.get("/logout", (req, res) => {
+    res.render("logout");
+})
+
+router.get("*", (req, res) => {
+    res.status(404).render("error", {authenticated: req.session.username !== undefined});
+})
+
+module.exports = router;
