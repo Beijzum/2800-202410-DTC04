aee980f7c4214b5af612b943c71feee0123236b8
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-05-10 11:36:31 -0700

    add front js for backend auth

---
+++ public/signUp.js
@@ -0,0 +1,24 @@
+document.getElementById("signUpForm").addEventListener("submit", async (e) => {
+    // stop page from refreshing before handle is done
+    e.preventDefault();
+
+    // get field entries
+    let username = document.getElementById("usernameField").value;
+    let email = document.getElementById("emailField").value;
+    let password = document.getElementById("passwordField").value;
+
+    let response = await fetch(backendLink + "/createAccount", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json" 
+        },
+        body: JSON.stringify({
+            username: username,
+            email: email,
+            password: password,
+        })
+    });
+    
+    window.location.href = response.url;
+});
