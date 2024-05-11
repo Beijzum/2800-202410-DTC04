3d3127444b54d72424abb53c1490e9996f59b849
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-05-10 23:59:20 -0700

    add retrieval of leaderboard in index

--- pageRoutes.js
+++ pageRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const database = require("./database");
 
-router.get("/", (req, res) => {
-    res.render("index", {authenticated: req.session.username !== undefined})
+router.get("/", async (req, res) => {
+    let top10Players = await database.getLeaderboard();
+    res.render("index", {authenticated: req.session.username !== undefined, topUsers: top10Players});
 });
 
 router.get("/game", (req, res) => {
