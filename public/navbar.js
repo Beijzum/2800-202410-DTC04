b3a0da07acb8bcf2411989b22d2ea64f8300fa08
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-05-11 00:58:44 -0700

    add functionality to hamburger button

---
+++ public/navbar.js
@@ -0,0 +1,7 @@
+let menuToggleButton = document.getElementById("hamburgerButton");
+
+menuToggleButton.addEventListener("click", () => {
+    let navbarItems = document.getElementById("navbarItems");
+    if (navbarItems.className.includes("hidden")) navbarItems.className = navbarItems.className.replace("hidden", "block");
+    else navbarItems.className = navbarItems.className.replace("block", "hidden");
+})
