036be74a46b8363481c93be5b7d6d87bf5f16cd5
Jason Chow <jchow149@my.bcit.ca>
2024-05-14 23:34:09 -0700

    ADD .env port to websocketImport (temp fix)

--- pageRoutes.js
+++ pageRoutes.js
@@ -1,31 +1,33 @@
+require('dotenv').config();
 const express = require("express");
 const router = express.Router();
 const database = require("./database");
+const port = process.env.PORT || 3000;
 
 router.get("/", async (req, res) => {
     let top10Players = await database.getLeaderboard();
-    res.render("index", {authenticated: req.session.username !== undefined, topUsers: top10Players});
+    res.render("index", { authenticated: req.session.username !== undefined, topUsers: top10Players });
 });
 
 router.get("/game", (req, res) => {
     // redirect to login if unauthenticated
-    if (req.session.username) res.render("game", {authenticated: true})
+    if (req.session.username) res.render("game", { authenticated: true, port })
     else res.redirect("/login");
 });
 
 router.get("/lobby", (req, res) => {
-    if (req.session.username) res.render("lobby", {authenticated: true});
+    if (req.session.username) res.render("lobby", { authenticated: true, port });
     else res.redirect("/login");
 });
 
 router.get("/login", (req, res) => {
     if (req.session.username) res.redirect("/index");
-    else res.render("login", {authenticated: false});
+    else res.render("login", { authenticated: false });
 })
 
 router.get("/signup", (req, res) => {
     if (req.session.username) res.redirect("/index");
-    else res.render("signup", {authenticated: false});
+    else res.render("signup", { authenticated: false });
 })
 
 router.get("/profile", async (req, res) => {
@@ -33,8 +35,8 @@ router.get("/profile", async (req, res) => {
         res.redirect("/login");
         return;
     }
-    let userData = await database.findUser({username: req.session.username});
-    res.render("profile", {authenticated: true, session: req.session, data: {winCount: userData.winCount, loseCount: userData.loseCount}});
+    let userData = await database.findUser({ username: req.session.username });
+    res.render("profile", { authenticated: true, session: req.session, data: { winCount: userData.winCount, loseCount: userData.loseCount } });
 
 })
 
@@ -45,7 +47,7 @@ router.get("/logout", (req, res) => {
 })
 
 router.get("*", (req, res) => {
-    res.status(404).render("404", {authenticated: req.session.username !== undefined});
+    res.status(404).render("404", { authenticated: req.session.username !== undefined });
 })
 
 module.exports = router;
