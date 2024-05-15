422e7e0d46320d0059606d13b8b8f78cf0e908d0
Jason Chow <jchow149@my.bcit.ca>
2024-05-15 00:13:19 -0700

    ADD player tracking. WIP

--- websocket.js
+++ websocket.js
@@ -1,5 +1,14 @@
 function runSocket(io) {
     io.on("connection", (socket) => {
+        // Player joins chatroom lobby
+        socket.on("joinLobby", (player) => {
+            // Players are given unique ID
+            const playerId = socket.id;
+            // playerName, playerProfile, and ready status is false
+            players[playerId] = { id: playerId, playerName: player.playerName, playerProfile: player.playerProfile, ready: false };
+            // Show other players that a new player has joined.
+            io.emit("playerJoined", players[playerId]);
+        });
 
         socket.on("message", (message) => {
             io.emit("message", message);
