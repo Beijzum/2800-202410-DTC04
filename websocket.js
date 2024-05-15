ceeedc72335616a27f45ffc7c3e7866e6693393d
Jason Chow <jchow149@my.bcit.ca>
2024-05-15 00:46:09 -0700

    ADD messages for joining/leaving room

--- websocket.js
+++ websocket.js
@@ -1,5 +1,9 @@
 function runSocket(io) {
     io.on("connection", (socket) => {
+        // When current user joins the chat
+        socket.emit("message", "You have joined a room")
+        // When any other user joins the chat
+        socket.broadcast.emit("message", "User has joined the chat")
         // Player joins chatroom lobby
         socket.on("joinLobby", (player) => {
             // Players are given unique ID
@@ -7,11 +11,15 @@ function runSocket(io) {
             // playerName, playerProfile, and ready status is false
             players[playerId] = { id: playerId, playerName: player.playerName, playerProfile: player.playerProfile, ready: false };
             // Show other players that a new player has joined.
-            io.emit("playerJoined", players[playerId]);
         });
 
         socket.on("message", (message) => {
             io.emit("message", message);
+        })
+
+        // When other users disconnect
+        socket.on("disconnect", () => {
+            io.emit("message", "A user has disconnected");
         })
     })
 }
