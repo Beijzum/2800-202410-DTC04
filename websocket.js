003bf9dcd5ae1b1b9d16c13a87a9217d937a3294
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-05-15 17:13:35 -0700

    refactor socket code for better scalability

--- websocket.js
+++ websocket.js
@@ -1,28 +1,30 @@
 /* Part of code from: https://socket.io/docs/v4/, https://www.youtube.com/watch?v=jD7FnbI76Hg */
 
+const gameHandler = require("./gameHandler");
+
+var readyList = [];
+
 function runSocket(io) {
     io.on("connection", (socket) => {
-        // When current user joins the chat
-        socket.emit("message", "You have joined a room")
-        // When any other user joins the chat
-        socket.broadcast.emit("message", "User has joined the chat")
+
         // Player joins chatroom lobby
-        socket.on("joinLobby", (player) => {
-            // Players are given unique ID
-            const playerId = socket.id;
-            // playerName, playerProfile, and ready status is false
-            players[playerId] = { id: playerId, playerName: player.playerName, playerProfile: player.playerProfile, ready: false };
-            // Show other players that a new player has joined.
+        socket.on("joinLobby", () => {
+            socket.emit("message", "You have joined a room");
+            socket.broadcast.emit("message", `${socket.request.session.username} has joined the room`);
         });
 
+        // when users message
         socket.on("message", (message) => {
             io.emit("message", message);
         })
 
         // When other users disconnect
         socket.on("disconnect", () => {
-            io.emit("message", "A user has disconnected");
+            io.emit("message", `${socket.request.session.username} has disconnected`);
         })
+
+        // Delegate game logic sockets to external module
+        gameHandler.runGame(socket);
     })
 }
 
