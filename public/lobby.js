21366c165ed5e4b064ed7a052a785391214db733
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-05-15 17:15:56 -0700

    add listener for when game starts

--- public/lobby.js
+++ public/lobby.js
@@ -1,16 +1,17 @@
-const playersReady = () => players.every(player => player.ready);
-
 let readyButton = document.getElementById("readyButton");
 
 readyButton.addEventListener("click", () => {
+    // player clicks ready
     if (readyButton.className.includes("bg-red-600")) {
         readyButton.className = readyButton.className.replace(/red-500/g, "green-500");
         readyButton.className = readyButton.className.replace(/red-600/g, "green-600");
     } else {
+        // player unreadies
         readyButton.className = readyButton.className.replace(/green-500/g, "red-500");
         readyButton.className = readyButton.className.replace(/green-600/g, "red-600");
     }
-    if (playersReady()) {
-        window.location.href = "/game";
-    }
+})
+
+socket.on("startGame", () => {
+    window.location.href = "/game";
 })
