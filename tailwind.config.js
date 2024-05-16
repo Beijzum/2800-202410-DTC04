992b3ff687dc96996fab1d450e44330195ed47fc
Richard Maceda <richardmaceda204@gmail.com>
2024-05-16 11:18:55 -0700

    fix merge conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: ["*"],
   theme: {
     extend: {
@@ -9,12 +8,5 @@ module.exports = {
         'danfo': ['Danfo', 'sans-serif'],
       }
     },
-=======
-  content: [],
-  theme: {
-    extend: {},
->>>>>>> d7068a6a6eacb971204116f8a059da5d5d8e7601
-  },
-  plugins: [],
-}
-
+  plugins: []
+}}
