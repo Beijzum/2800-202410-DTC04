63ed987c438a1ffd5eaab4670106a3eaff59d314
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-05-15 22:26:49 -0700

    add iterator cycle for better state saving

--- gameHandler.js
+++ gameHandler.js
@@ -1,11 +1,34 @@
+const phases = ["write", "vote", "result", "wait"];
+var currentPhase = cycle(phases);
+var gameRunning;
+
 function runGame(io, socket) {
     socket.on("joinGame", () => {
-        console.log("player joined");
-    })
+        if (!io.sockets.adapter.rooms.get("game")) gameRunning = false;
+
+        console.log(gameRunning);
+        console.log(currentPhase.next().value);
+        console.log(currentPhase.next().value);
+        console.log(currentPhase.next().value);
+        console.log(currentPhase.next().value);
+        console.log(currentPhase.next().value);
+    });
+
+    
 }
 
+function* cycle(array) {
+    let index = 0;
+    while (true) {
+        yield array[index];
+        index = (index + 1) % array.length;
+    }
+}
 
-
+function resetGame() {
+    currentPhase = phases[0];
+    gameRunning = false;
+}
 
 
 module.exports = {
