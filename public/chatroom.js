c1e68cd680aaa74396430b2d3c2b6283c3a6a9ce
Jason Chow <jchow149@my.bcit.ca>
2024-05-17 03:49:35 -0700

    ADD systemMessage listener in chatroom.js. ADJUST css for messages

--- public/chatroom.js
+++ public/chatroom.js
@@ -10,13 +10,15 @@ let messageSection = document.getElementById("messageSection");
 socket.on("message", (messageContent) => {
     const message = document.createElement("li");
     message.innerHTML = `
-<div class="max-w-lg ml-3 my-3 p-3 rounded-lg intense-shadow_2 text-lg">
-        <div class="flex justify-between bg-blue-500 p-2 rounded-t-lg">
+    <div class="max-w-lg ml-3 my-3 p-3 rounded-lg intense-shadow_2 text-lg">
+        <div class="flex justify-between items-center bg-blue-500 p-2 rounded-t-lg">
             <div class="flex items-center">
                 ${messageContent.profilePic ? `<img src="${messageContent.profilePic}" class="w-8 h-8 rounded-full mr-3">` : ""}
                 <span class="font-bold text-white">${messageContent.username}</span>
             </div>
-            <span class="text-xs text-black">${messageContent.time}</span>
+            <div class="flex-grow flex justify-end">
+                <span class="text-xs text-black">${messageContent.time}</span>
+            </div>
         </div>
         <div class="bg-gray-300 text-black p-2 rounded-b-lg whitespace-normal break-words">
             ${messageContent.text}
@@ -25,7 +27,28 @@ socket.on("message", (messageContent) => {
     `;
     messageSection.appendChild(message);
     window.scrollTo(0, document.body.scrollHeight);
-})
+});
+
+socket.on("systemMessage", (messageContent) => {
+    const message = document.createElement("li");
+    message.innerHTML = `
+    <div class="max-w-lg ml-3 my-3 p-3 rounded-lg intense-shadow_2 text-lg">
+        <div class="flex justify-between items-center bg-red-500 p-2 rounded-t-lg">
+            <div class="flex items-center">
+                <span class="font-bold text-white">System Message</span>
+            </div>
+            <div class="flex-grow flex justify-end">
+                <span class="text-xs text-black">${messageContent.time}</span>
+            </div>
+        </div>
+        <div class="bg-gray-300 text-black p-2 rounded-b-lg whitespace-normal break-words">
+            ${messageContent.text}
+        </div>
+    </div>
+    `;
+    messageSection.appendChild(message);
+    window.scrollTo(0, document.body.scrollHeight);
+});
 
 // SENDING SOCKET DATA SECTION
 
