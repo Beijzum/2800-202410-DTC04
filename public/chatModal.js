015a5b46a2c0d12ad45a71123e7e7afd36e0b231
Jaiden Duncan <jaiden_d_duncan@yahoo.com>
2024-05-16 22:58:52 -0700

    Change modal to be dynamically added to the page

--- public/chatModal.js
+++ public/chatModal.js
@@ -3,38 +3,54 @@ const card = document.createElement("div");
 card.appendChild(document.createElement("p"));
 card.classList.add("flex", "flex-row", "py-2", "px-4", "w-full");
 
-// handle receiving the updated user list
-socket.on("updateUserList", (users) => {
-    const modal = document.querySelector("#userDisplays");
+// init the modal
+document.addEventListener("DOMContentLoaded", (_) => {
+    const modal = document.createElement('dialog');
+    modal.id = "userDisplays";
+    modal.className = "flex flex-col w-1/3 h-2/3 overflow-y-auto";
+    document.body.appendChild(modal);
 
-    let user_arr = users;
-    const userIds = [];
-    // Removes all users that have left
-    modal.childNodes.forEach(function(node) {
-        if (!user_arr.includes(node.id)) {
-            node.remove();
+    modal.addEventListener("click", function(e) {
+        if (e.target === modal) {
+            modal.close();
         }
-    })
-    // filters out all players still in game
-    user_arr = user_arr.filter(user => !userIds.includes(user));
-    // adds card a card for every new player
-    user_arr.forEach(user => {
-        let userCard = card.cloneNode(true);
-        
-        userCard.id = user;
-        userCard.querySelector("p").innerText = user;
-        modal.appendChild(userCard);
     });
-});
 
-// click event on the player list icon
-document.querySelector("#playerListButton").addEventListener("click", async function(e) {
-    // so user's don't randomly click it
-    if (this.style.display === "none") {
-        return;
-    }
+    const userCards = [];
+    
+    // handle receiving the updated user list
+    socket.on("updateUserList", (users) => {      
+        const userIds = [];
+        // Removes all users that have left
+        modal.childNodes.forEach(function(node) {
+            if (!users.includes(node.id)) {
+                node.remove();
+            }
+        })
+        // filters out all players still in game
+        const user_arr = users.filter(user => !userIds.includes(user));
+        // adds card a card for every new player
+        user_arr.forEach(user => {
+            let userCard = card.cloneNode(true);
+            
+            userCard.id = user;
+            userCard.querySelector("p").innerText = user;
+            userCards.push(userCard);
+        });
+    });
+    
+    // click event on the player list icon
+    document.querySelector("#playerListButton").addEventListener("click", async function(e) {
+        // so user's don't randomly click it
+        if (this.style.display === "none") {
+            return;
+        }
 
-    console.log(`
-    <dialog id="userDisplays" class="flex flex-col w-1/3 h-5/6 overflow-y-auto"></dialog>
-    `)
-});
+        while (modal.firstChild) {
+            modal.removeChild(modal.firstChild);
+        }
+
+        userCards.map(card => modal.appendChild(card));
+        modal.showModal();
+    });
+})
