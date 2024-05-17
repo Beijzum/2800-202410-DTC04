4ae5abc995815dad232313f773e75130bc208ff8
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-05-17 06:22:52 -0700

    add cycling pages, need to fix wait running twice

--- gameHandler.js
+++ gameHandler.js
@@ -5,7 +5,7 @@ const EventEmitter = require("events").EventEmitter;
 const ee = new EventEmitter(); // used for passing control from server to self
 
 // PHASES: WRITE, VOTE, RESULT, WAIT
-var currentPhase, gameRunning = false, promptIndex, timerValue, timer, round;
+var currentPhase, gameRunning = false, promptIndex, phaseDuration, round;
 
 // CHECK TO SEE IF REDIRECTING TO NEW PAGE CAUSES A NEW SOCKET SESSION TO BE CREATE ---> DIFFERENT ID SO PLAYERS WONT BE COUNTED AS "IN-GAME"
 
@@ -19,6 +19,8 @@ const waitTemplate = fs.readFileSync("./socketTemplates/wait.ejs", "utf8");
 const resultTemplate = fs.readFileSync("./socketTemplates/result.ejs", "utf8");
 
 function runGame(io, socket) {
+
+    // player connects to game lobby
     socket.on("joinGame", () => {
         socket.join("game"); // keep just for testing purposes, remove later
         
@@ -32,7 +34,11 @@ function runGame(io, socket) {
         // user has joined and is part of the game, and is the first to join
         if (gameRunning === false) {
             gameRunning = true;
+            round = 1;
         }
+
+        // send round details to frontend
+        socket.emit("roundUpdate", round);
 
         // user has joined, and is part of the game
         if (socket.rooms.has("game")) {
@@ -42,20 +48,16 @@ function runGame(io, socket) {
             switch(currentPhase) {
                 // jump to whatever screen the game is currently on
                 case "WRITE":
-                    let renderedWriteTemplate = ejs.render(writeTemplate);
-                    socket.emit("changeView", renderedWriteTemplate);
+                    ee.emit("WRITE");
                     break;
                 case "VOTE":
-                    let renderedVoteTemplate = ejs.render(voteTemplate);
-                    socket.emit("changeView", renderedVoteTemplate);
+                    ee.emit("VOTE");
                     break;
                 case "RESULT":
-                    let renderedResultTemplate = ejs.render(resultTemplate);
-                    socket.emit("changeView", renderedResultTemplate);
+                    ee.emit("RESULT");
                     break;
                 case "WAIT":
-                    let renderedWaitTemplate = ejs.render(waitTemplate);
-                    socket.emit("changeView", renderedWaitTemplate);
+                    ee.emit("WAIT");
                     break;
                 default:
                     let renderedNoGameTemplate = ejs.render(noGameTemplate);
@@ -64,31 +66,87 @@ function runGame(io, socket) {
         }
     });
 
+    // handle logic for write screen
     ee.on("runWrite", () => {
+        currentPhase = "WRITE";
+
+        // only generate new prompt when first person joins
         if (!promptIndex) promptIndex = Math.floor(Math.random() * pool.prompts.length);
+
+        // update frontend UI
         let renderedWriteTemplate = ejs.render(writeTemplate, {prompt: pool.prompts[promptIndex]});
-        io.emit("changeView", renderedWriteTemplate);
-        if (!timer) {
-            timerValue = 60;
-            createTimer();
-            // let renderedVoteTemplate = ejs.render(voteTemplate)
-            // createDelayedRedirect(timerValue + 1, () => { io.emit("changeView", )})
+        socket.emit("changeView", renderedWriteTemplate);
+
+        // only run when the first user connects
+        if (!phaseDuration || phaseDuration <= 0) {
+            phaseDuration = 5;
+            let timer = setInterval(updateClientTimers, 1000);
+            createDelayedRedirect(phaseDuration + 1, timer, "runVote");
         }
     });
 
+    // handle logic for vote screen
     ee.on("runVote", () => {
+        currentPhase = "VOTE";
+        let renderedTemplate = ejs.render(noGameTemplate);
+        socket.emit("changeView", renderedTemplate);
+
+        // let renderedVoteTemplate = ejs.render(renderedVoteTemplate, {});
+        // socket.emit("changeView", renderedVoteTemplate);
         
+        if (phaseDuration <= 0) {
+            console.log('vote')
+            phaseDuration = 5;
+            let timer = setInterval(updateClientTimers, 1000);
+            createDelayedRedirect(phaseDuration + 1, timer, "runResult");
+        }
     });
 
+    // handle logic for result screen
     ee.on("runResult", () => {
+        currentPhase = "RESULT"
 
+        let renderedTemplate = ejs.render(noGameTemplate);
+        socket.emit("changeView", renderedTemplate);
+
+        // let renderedResultTemplate = ejs.render(renderedResultTemplate, {});
+        // socket.emit("changeView", renderedResultTemplate);
+        
+        if (phaseDuration <= 0) {
+            console.log('result')
+            phaseDuration = 5;
+            let timer = setInterval(updateClientTimers, 1000);
+            createDelayedRedirect(phaseDuration + 1, timer, "runWait");
+        }
     })
 
+    // handle logic for wait screen
     ee.on("runWait", () => {
+        currentPhase = "WAIT";
         if (!io.sockets.adapter.rooms.get("game") || io.sockets.adapter.rooms.get("game").size === 0)
             stopGame();
+
+        // ADD LOGIC TO HANDLE PLAYERS WIN OR LOSE HERE
+
+        // move onto rendering page if game has not ended
+        let renderedWaitTemplate = ejs.render(waitTemplate);
+        socket.emit("changeView", renderedWaitTemplate);
+        socket.emit("roundUpdate", round);
+
+        // move onto next round
+        if (phaseDuration <= 0) {
+            console.log('wait')
+            phaseDuration = 5;
+            round++;
+            setupNextRound();
+            let timer = setInterval(updateClientTimers, 1000);
+            createDelayedRedirect(phaseDuration + 1, timer, "runWrite");
+        }
     })
 
+    // CLIENT LISTENER SECTION
+
+    // when player submit response
     socket.on("submitResponse", () => {
 
     });
@@ -97,20 +155,18 @@ function runGame(io, socket) {
 
     // FUNCTION DEFINITIONS THAT REQUIRE IO 
 
-    function createDelayedRedirect(delayTimeInSeconds, functionToUse) {
-        setTimeout(functionToUse, delayTimeInSeconds * 1000);
+    function createDelayedRedirect(delayTimeInSeconds, timer, nextRoute) {
+        setTimeout(() => {
+            ee.emit(nextRoute);
+            clearInterval(timer);
+        }, delayTimeInSeconds * 1000);
     }
 
-    function createTimer() {
-        timer = setInterval(() => {
-            if (timerValue <= 0) {
-                clearInterval(timer);
-                timer = null;
-            } else {
-                timerValue--;
-                io.emit("timerUpdate", convertFormat(timerValue));
-            }
-        }, 1000);
+    function updateClientTimers() {
+        if (phaseDuration > 0) {
+            phaseDuration--;
+            io.emit("timerUpdate", convertFormat(phaseDuration));
+        }
     }
 }
 
@@ -120,11 +176,17 @@ function convertFormat(seconds) {
     return `${Math.floor(seconds / 60)}:${seconds < 10 ? "0" + seconds % 60 : seconds % 60}`;
 }
 
+function setupNextRound() {
+    promptIndex = null;
+    timer = null;
+    phaseDuration = null;
+}
+
 function stopGame() {
     currentPhase = null;
     gameRunning = false;
     promptIndex = null;
-    timerValue = null;
+    phaseDuration = null;
     timer = null;
     round = null;
 }
