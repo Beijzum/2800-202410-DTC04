b0fbec8dba691a67f3de768b0638b8db3514e2c0
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-05-23 02:16:47 -0700

    adjust button color change when ready is pressed

--- public/lobby.js
+++ public/lobby.js
@@ -1,29 +1,44 @@
+var ready = false;
 let readyButton = document.getElementById("readyButton");
 let navbarMessage = document.getElementById("gameNavbarMessage");
 
 readyButton.addEventListener("click", () => {
     // player readies
-    if (!isReady()) {
-        readyButton.className = readyButton.className.replace(/red-500/g, "green-500");
-        readyButton.className = readyButton.className.replace(/red-600/g, "green-600");
+    if (!ready) {
+        // switch background color
+        readyButton.className = readyButton.className.replace(/sky-700/g, "zinc-200");
+        // switch hover background color
+        readyButton.className = readyButton.className.replace(/sky-600/g, "zinc-100");
+        // switch border color
+        readyButton.className = readyButton.className.replace(/sky-500/g, "zinc-400");
+        // switch hover border color
+        readyButton.className = readyButton.className.replace(/sky-400/g, "zinc-300");
+        readyButton.className = readyButton.className.replace("text-white", "text-gray-700");
+        readyButton.innerHTML = "Unready";
+        ready = true;
         socket.emit("ready");
     } else {
-        // player unreadies
-        readyButton.className = readyButton.className.replace(/green-500/g, "red-500");
-        readyButton.className = readyButton.className.replace(/green-600/g, "red-600");
-        navbarMessage.innerHTML = "Waiting for Game to Start...";
+        // undo all the changes did when ready was pressed
+         // switch background color
+         readyButton.className = readyButton.className.replace(/zinc-200/g, "sky-700");
+         // switch hover background color
+         readyButton.className = readyButton.className.replace(/zinc-100/g, "sky-600");
+         // switch border color
+         readyButton.className = readyButton.className.replace(/zinc-400/g, "sky-500");
+         // switch hover border color
+         readyButton.className = readyButton.className.replace(/zinc-300/g, "sky-400");
+         readyButton.className = readyButton.className.replace("text-gray-700", "text-white");
+         readyButton.innerHTML = "Ready";
+         navbarMessage.innerHTML = "Waiting for Game to Start...";
+         ready = false;
         socket.emit("unready");
     }
 })
 
 socket.on("updateReadyMessage", (readyMessage) => {
-    if (!isReady()) return;
+    if (!ready) return;
     navbarMessage.innerHTML = readyMessage;
 });
-
-function isReady() {
-    return document.getElementById("readyButton").className.includes("bg-green-600");
-}
 
 socket.on("startGame", () => {
     socket.emit("forceJoin");
