e2a2edfc4be840d2b0e4e5b835fd67654f24b7ee
Jaiden Duncan <jaiden_d_duncan@yahoo.com>
2024-05-23 02:10:51 -0700

    Add Contra gif to easter egg page

--- public/memes.js
+++ public/memes.js
@@ -2,6 +2,7 @@
 let youLikeJazz = new Audio("sfx/jazz.mp3");
 let no = new Audio("sfx/no.mp3");
 let robot = new Audio("sfx/robot.mp3");
+let heli = new Audio("sfx/heliAudio.mp3");
 
 // CLICK EVENT LISTENERS HERE
 document.getElementById("jazz").addEventListener("click", () => {
@@ -14,4 +15,13 @@ document.getElementById("no").addEventListener("click", () => {
 
 document.getElementById("robot").addEventListener("click", () => {
     robot.play();
+});
+
+document.getElementById("heli").addEventListener("click", () => {
+    let heliImg = document.getElementById("heli");
+    heliImg.src = "images/heli.gif";
+    heli.play();
+    setTimeout(() => {
+        heliImg.src = "images/heli.png";
+    }, 7500);
 });
