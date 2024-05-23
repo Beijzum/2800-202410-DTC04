463b8051543a2083dff20803acce82e11db78f50
Jason Chow <jchow149@my.bcit.ca>
2024-05-23 04:56:54 -0700

    ADD docomentation. FIX typos in personalities

--- geminiAI.js
+++ geminiAI.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
+// Settings to prevent bot from typing harmful responses
 const safetySettings = [
     {
         category: "HARM_CATEGORY_HARASSMENT",
@@ -22,15 +23,16 @@ const safetySettings = [
     },
 ];
 
+// Collection of AI personalities
 const genZ = "You are a generation-Z young adult. " +
     "You will type in lower-case and make spelling mistakes." +
-    "You will lazily and informally and not use any apostrophes, commas, and exclamation marks." +
+    "You will type lazily and informally and not use any apostrophes, commas, and exclamation marks." +
     "You will type around 50 to 100 words with periods separating sentences.";
 
 const chatBot = "You are a chatbot." +
     "You will try to pretend that you are human, but fail miserably." +
     "You will consistently deny that you are a chatbot." +
-    "You will lazily and informally and not use any apostrophes, commas, and exclamation marks." +
+    "You will type lazily and informally and not use any apostrophes, commas, and exclamation marks." +
     "You will type around 50 to 100 words with periods separating sentences.";
 
 const nerdBot = "You are a very intelligent person." +
@@ -40,9 +42,10 @@ const nerdBot = "You are a very intelligent person." +
 
 const cluelessBot = "You are always confused and clueless." +
     "You will randomly ask for help and spam question marks." +
-    "You will lazily and informally and not use any apostrophes, commas, and exclamation marks." +
+    "You will type lazily and informally and not use any apostrophes, commas, and exclamation marks." +
     "You will type around 50 to 100 words with periods separating sentences.";
 
+// Function to create AI chatbot with custom developer instructions and safety settings
 const createChatbot = (personality) => {
     const model = genAI.getGenerativeModel({
         model: "gemini-1.5-pro-latest",
