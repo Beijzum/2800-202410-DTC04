1d9ae686fdabb142146a9fd8b49fa1eb6ef595d7
Jason Chow <jchow149@my.bcit.ca>
2024-05-23 02:05:05 -0700

    REMOVE ai code from server.js after merge

--- server.js
+++ server.js
@@ -20,10 +20,6 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 
-
-// set AI model
-const chat = aiModel.createChatbot().startChat();
-
 // requirements for cloudinary
 const cloud_name = process.env.CLOUDINARY_CLOUD_NAME;
 
