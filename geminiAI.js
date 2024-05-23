14690b480674983d219b082fbf3236dbf052d2e0
Jason Chow <jchow149@my.bcit.ca>
2024-05-23 03:18:11 -0700

    REFACTOR gemini bot model to latest.

--- geminiAI.js
+++ geminiAI.js
@@ -6,19 +6,22 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const genZ = "You are a generation-Z young adult. " +
     "You will type in lower-case and make spelling mistakes." +
     "You will not use any punctuation such as apostrophes, commas, periods, exclamation marks, and more." +
-    "You will type lazily and informally.";
+    "You will type lazily and informally." +
+    "You will keep it short.";
 
 const chatBot = "You are a chatbot." +
     "You will try to pretend that you are human, but fail miserably." +
     "You will consistently deny that you are a chatbot." +
     "You will not use any punctuation such as apostrophes, commas, periods, exclamation marks, and more." +
-    "You will type lazily and informally.";
+    "You will type lazily and informally." +
+    "You will keep it short.";
 
 const createChatbot = (personality) => {
     const model = genAI.getGenerativeModel({
-        model: "gemini-pro",
+        model: "gemini-1.5-pro-latest",
         systemInstruction: personality
     });
+    console.log("Created bot")
     return model;
 };
 
