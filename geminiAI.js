2510a66767911a906a6b09905394df081b7a5db2
Jason Chow <jchow149@my.bcit.ca>
2024-05-23 03:47:19 -0700

    ADD proper imports for GoogleGenerativeAI harm block/thresh

--- geminiAI.js
+++ geminiAI.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const safetySettings = [
