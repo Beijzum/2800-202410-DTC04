1f6594863a4b5227a5ff51d9165b099ee4cdd7ec
Jaiden Duncan <jaiden_d_duncan@yahoo.com>
2024-05-23 23:14:28 -0700

    Change volume of heli audio to be quieter

--- public/memes.js
+++ public/memes.js
@@ -4,6 +4,8 @@ let no = new Audio("sfx/no.mp3");
 let robot = new Audio("sfx/robot.mp3");
 let heli = new Audio("sfx/heliAudio.mp3");
 let nextGen = new Audio("sfx/suspicious.mp3");
+// It's REALLY loud
+heli.volume = 0.5;
 
 // CLICK EVENT LISTENERS HERE
 document.getElementById("nextGen").addEventListener("click", () => {
