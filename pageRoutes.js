f507ffaa3427b1ca400559732705f7b7b45771c6
Richard Maceda <richardmaceda204@gmail.com>
2024-05-29 23:18:49 -0700

    remove leaderboard getting pass to index

--- pageRoutes.js
+++ pageRoutes.js
@@ -7,8 +7,8 @@ router.get("/", async (req, res) => {
     let top10Players = await database.getLeaderboard();
     if (req.session.username) {
         let userData = await database.findUser({ username: req.session.username });
-        res.render("index", { authenticated: true, data: { winCount: userData.winCount, loseCount: userData.loseCount }, topUsers: top10Players, userExist: { isTrue : true }, sessionData: req.session });
-    } else { res.render("index", { authenticated: req.session.username !== undefined, topUsers: top10Players, data: { winCount: 0 , loseCount: 0 }, userExist: { isTrue : false  }  }) };
+        res.render("index", { authenticated: true, data: { winCount: userData.winCount, loseCount: userData.loseCount }, userExist: { isTrue : true }, sessionData: req.session });
+    } else { res.render("index", { authenticated: req.session.username !== undefined, data: { winCount: 0 , loseCount: 0 }, userExist: { isTrue : false  }  }) };
 });
 
 router.get("/game", (req, res) => {
