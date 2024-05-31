6d8c1abbe63f1e2321fb5d685e4a4594f21f156f
Jason Chow <jchow149@my.bcit.ca>
2024-05-31 01:35:33 -0700

    FIXED jshint errors in navbar.js

--- public/navbar.js
+++ public/navbar.js
@@ -4,4 +4,4 @@ menuToggleButton.addEventListener("click", () => {
     let navbarItems = document.getElementById("navbarItems");
     if (navbarItems.className.includes("hidden")) navbarItems.className = navbarItems.className.replace("hidden", "block");
     else navbarItems.className = navbarItems.className.replace("block", "hidden");
-})
+});
